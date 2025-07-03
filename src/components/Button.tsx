924b9848740e5a4686f7e196eb6d7e7d55ce5ee0
Donya <d4nyaras@gmail.com>
2025-07-03 20:41:22 +0330

    Add: Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -28,7 +28,7 @@ export default function Button({
       disabled={disable}
       className={`disabled: opacity-70 disabled:cursor-not-allowed rounded-md hover:opacity-80 transition  w-full  flex items-center justify-center gap-2 ${
         outline ? "bg-white" : "bg-[#FB2873]"
-      } ${outline ? "text-[#FB2873]" : "text-white"} ${
+      } ${outline ? "text-[#eb286c]" : "text-white"} ${
         small ? "text-sm font-light" : "text-md font-semibold"
       }  ${small ? "py-1 px-2 border-[1px]" : "py-3 px-4 border-2"} ${
         custom ? custom : ""
